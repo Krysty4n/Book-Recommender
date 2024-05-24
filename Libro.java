5a152d0d6f691d6cf526a05a685f36ee2250fb2a
Krysty4n <129935002+Krysty4n@users.noreply.github.com>
2024-05-24 10:33:41 +0200

    Add files via upload

---
+++ Libro.java
@@ -0,0 +1,13 @@
+public class Libro {
+    private String titolo;
+    private String autore;
+    private int annoPubblicazione;
+
+    public Libro(String titolo, String autore, int annoPubblicazione){
+        this.titolo = titolo;
+        this.autore = autore;
+        this.annoPubblicazione = annoPubblicazione;
+
+    }
+
+}
